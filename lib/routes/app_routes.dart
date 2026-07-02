4cf8a24dab6ce022c937296c04ee6fd0980ac741
juanfranciscovm <jvillarroel.1184@unimar.edu.ve>
2026-07-02 12:44:35 -0400

    initialize repository: inicialización del repositorio y creación de carpetas y archivos base.

---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,10 @@
+import "package:flutter/material.dart";
+import "package:cotufaverse/screens/app_screens.dart";
+
+class AppRoutes {
+  static const initialRoute = "/home";
+
+  //static Map<String, Widget Function(BuildContext)> routes = {
+  //"/home": (BuildContext context) => const blabla(),
+  //};
+}
