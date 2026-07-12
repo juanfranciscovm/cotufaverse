fc895bc03a321a07d8acee7f244b853a139d06c1
Acthel12 <160182015+Acthel12@users.noreply.github.com>
2026-07-12 19:24:21 -0400

    feat: agregar clase PosterMovieCard y actualizar exports en widgets.dart

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,3 +1,4 @@
 export 'package:cotufaverse/widgets/preview_swiper.dart';
 export "package:cotufaverse/widgets/nav_bar.dart";
 export "package:cotufaverse/widgets/card_swiper.dart";
+export "package:cotufaverse/widgets/poster_movie_card.dart";
