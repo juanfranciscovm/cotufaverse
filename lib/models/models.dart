7d3b02d442cfdc681750568406b3ad0cbe37974e
juanfranciscovm <jvillarroel.1184@unimar.edu.ve>
2026-07-12 20:04:20 -0400

    add: agregados los modelos para ser exportados en un solo archivo

---
+++ lib/models/models.dart
@@ -0,0 +1,4 @@
+export "package:cotufaverse/models/movie.dart";
+export "package:cotufaverse/models/playing.dart";
+export "package:cotufaverse/models/credits.dart";
+export "package:cotufaverse/models/popular.dart";
