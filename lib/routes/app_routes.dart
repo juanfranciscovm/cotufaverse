4454a4757dd3d29e184b608ae392eaab73d3f182
juanfranciscovm <jvillarroel.1184@unimar.edu.ve>
2026-07-19 21:42:58 -0400

    fix: arreglada la ruta de la favorites_screen

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,3 +1,4 @@
+import 'package:cotufaverse/screens/favorites_screen.dart';
 import 'package:flutter/material.dart';
 import "package:cotufaverse/screens/app_screens.dart";
 
@@ -11,5 +12,6 @@ class AppRoutes {
     "/profile": (BuildContext context) => const ProfileScreen(),
     "/details": (BuildContext context) => const DetailsScreen(),
     "/about_devs": (BuildContext context) => const AboutDevs(),
+    "/favorites_screen": (BuildContext context) => const FavoritesScreen(),
   };
 }
